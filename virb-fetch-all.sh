3a83eefe61028001f57ef8c8b8d335c9b506822d
Paweł Owczarek <pawel@ow.cx>
2017-08-24 18:09:18 +0200

    Delete an obsolete file

--- virb-fetch-all.sh
+++
@@ -1,2 +0,0 @@
-wget --post-data '{"command":"features"}' --header="Content-Type: application/json" -O virb-fetch-all-features.json http://192.168.0.1/virb
-wget --post-data '{"command":"status"}' --header="Content-Type: application/json" -O virb-fetch-all-status.json http://192.168.0.1/virb
